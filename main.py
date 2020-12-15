e2c3b6d4b44f0fcc7488d2c3b588ce9a78fd7142
kinnplh <kinnplh@126.com>
2020-12-15 15:53:17 +0800

    new generate methods

--- main.py
+++ main.py
@@ -58,6 +58,10 @@ def generate_task(user_name, class_list, length, last_time, noise_center, noise_
 
         if noise_size is None:
             noise_size = 100
+        
+        if noise_size < 1:
+            noise_size = int(noise_size * height)
+
         noise_left = max(0, int(noise_center_x - noise_size / 2))
         noise_top = max(0, int(noise_center_y - noise_size / 2))
         noise_right = min(int(noise_center_x + noise_size / 2), width)  # exclude
@@ -173,6 +177,47 @@ def confirm_answer():
         json.dump({'next_index': index + 1}, f)
     
     return jsonify(state='success')
+
+animals = ["llama", "cougar_body", "kangaroo", "wild_cat"]
+insect = ["ant", "scorpion", "crab", "crayfish"]
+buildings = ["buddha", "pyramid", "minaret", "pagoda"]
+furniture = ["menorah", "ewer", "wheelchair", "chair"]
+plants = ["water_lilly", "lotus", "bonsai", "strawberry"]
+
+name_to_list = {
+    'animals': animals,
+    'insect': insect,
+    'buildings': buildings,
+    'furniture': furniture,
+    'plants': plants
+}
+@app.route('/generate_task_for_user', methods=['GET'])
+def generate_task_for_user():
+    user_name = request.args.get('u')
+    class_names = list(name_to_list.keys())
+    last_times = [20, 30, 40, 80, 160]
+    random.shuffle(class_names)
+    random.shuffle(last_times)
+    noise_size = [0, 1/3]
+
+    task_names =[]
+
+    for i in range(5):
+        class_name = class_names[i]
+        last_time = last_times[i]
+
+        tn1 = generate_task("{}_{}_{}_33".format(user_name, class_name, last_time), name_to_list[class_name], 20, last_time, None, 1/3)
+        task_names.append(tn1)
+
+        tn2 = generate_task("{}_{}_{}_0".format(user_name, class_name, last_time), name_to_list[class_name], 20, last_time, None, 0)
+        task_names.append(tn2)
+    
+    result = ""
+
+    for tn in task_names:
+        result = result + "<p><a href='/task?tn={}' target='_blank'>{}</a></p>\n".format(tn, tn)
+
+    return result
 
 @app.route('/show_result', methods=['GET'])
 def show_result():
